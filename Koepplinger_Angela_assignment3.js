4eb0c705abde03ade87be4ce067326bdfa2a253c
angiekoepplinger <angiekoepplinger@fullsail.edu>
2012-08-15 21:16:17 -0500

    takeTrip
    console log passenger list, use as conditional for Boolean argument

--- Koepplinger_Angela_assignment3.js
+++ Koepplinger_Angela_assignment3.js
@@ -5,7 +5,7 @@
 
 
 var passengerList= [];
-
+//var takeTrip = 
 
 //Constructor
 var passenger = function (name,age,gender) {
@@ -13,6 +13,12 @@ var passenger = function (name,age,gender) {
 	var luggageItems = [];
 	
 	//Functions
+	var takeFlight= function (answer){
+		if (answer === true ) {
+		console.log("Ready to fly")
+		};
+	};
+
 	var addLuggage = function (item) {
 		for (var i =0; i < item.length; i++) {
 			if (gender === "Female") {
@@ -20,13 +26,10 @@ var passenger = function (name,age,gender) {
 			} else {
 				console.log(name + " has added " + item[i] + " to his luggage.");
 			}
-			luggageItems.push(item);
+			luggageItems.push(item[i]);
 		}
 	};
 	var showLuggage = function () { 
-		for ( var i = 0; i < luggageItems.length; i++) {
-			console.log(name + " packed " + luggageItems[i] + " for the trip.");
-		};
 		return luggageItems;
 	};
 	var getName = function () { 
@@ -35,9 +38,9 @@ var passenger = function (name,age,gender) {
 	
 	var getAge = function () {
 		if(age < 15) {
-			console.log(name + " must be accompanied by an adult on this flight")
+			console.log(name + " must be accompanied by an adult on this flight.")
 		} else {
-			console.log(name + " is an adult passenger")
+			console.log(name + " is an adult passenger.")
 		};
 		return age;
 	};
@@ -51,12 +54,13 @@ var passenger = function (name,age,gender) {
 		"age": getAge,		
 		"gender": getGender,
 		"addContents": addLuggage,
-		"showContents": showLuggage
+		"showContents": showLuggage,
+		"takeFlight": takeFlight
 
 	};
 
 };
-//family members names
+//Travelers Names
 var passenger1 = passenger("Angela",29,"Female");
 passengerList.push(passenger1);
 var passenger2 = passenger("Aaron",30,"Male");
@@ -64,19 +68,30 @@ passengerList.push(passenger2);
 var passenger3 = passenger("Sarah",11,"Female");
 passengerList.push(passenger3);
 
+if (passengerList.length === 3){
+	passenger1.takeFlight(true);
+} else {
+	passenger1.takeFlight(false);
+};
+ 
 
-passenger1.addContents(["bathing suits", "sun glasses"]);
-passenger2.addContents(["beach chairs", "snorkle", "towels"])
-passenger3.addContents(["sand toys", "umbrella", "crab net"]);
+passenger1.addContents(["bathing suits", "sun glasses", "flip flops, sunscreen"]);
+passenger2.addContents(["beach chairs", "snorkle", "towels", "toiletries"])
+passenger3.addContents(["sand toys", "umbrella", "crab net", "goggles", "kite"]);
+
+console.log(" ");
+
+console.log(passenger1.name() + "'s suitcase contains: " + passenger1.showContents()+ ".");
+console.log(passenger2.name() + "'s suitcase contains: " + passenger2.showContents()+ ".");
+console.log(passenger3.name() + "'s suitcase contains: " + passenger3.showContents()+ ".");
+
+console.log(" ");
 
 console.log(passenger1.name() + " is " + passenger1.age());
 console.log(passenger2.name() + " is " + passenger2.age());
 console.log(passenger3.name() + " is " + passenger3.age());
-
-
-//
-
-
+console.log(" ");
+console.log(passengerList[0].name() + passengerList[1].name() + passengerList[2].name());
 
 
 
